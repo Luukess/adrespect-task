7829a68da7c6ea35766588bd1e2fa5b300616f15
Lukasz Chwiedosik <lukasz.chwiedosik@gmail.com>
2023-08-17 19:22:54 +0200

    Feat: added nav mobile menu

--- scripts/navi/searchBox.js
+++ scripts/navi/searchBox.js
@@ -16,5 +16,47 @@ function handleSearchBox() {
     }
 };
 
+function handleMobileNav() {
+    const navLayer = document.getElementById('navi-layer');
+    const naviBody = document.getElementById('mobile-body-navi');
+    const headerNavi = document.getElementById('mobile-header-navi');
+    const widthOfNaviLayer = navLayer.offsetWidth;
+    if (widthOfNaviLayer !== 0) {
+        navLayer.classList.remove('navi-layer-visible');
+        navLayer.classList.add('navi-layer-hidden');
+        naviBody.classList.remove('mobile-navi-visible');
+        naviBody.classList.add('mobile-navi-hidden');
+        headerNavi.classList.remove('mobile-header-navi-visible');
+        headerNavi.classList.add('mobile-header-navi-hidden');
+    } else {
+        navLayer.classList.remove('navi-layer-hidden');
+        navLayer.classList.add('navi-layer-visible');
+        naviBody.classList.remove('mobile-navi-hidden');
+        naviBody.classList.add('mobile-navi-visible');
+        headerNavi.classList.remove('mobile-header-navi-hidden');
+        headerNavi.classList.add('mobile-header-navi-visible');
+    }
+}
+
+function handleResetMenuResize() {
+    const navLayer = document.getElementById('navi-layer');
+    const naviBody = document.getElementById('mobile-body-navi');
+    const headerNavi = document.getElementById('mobile-header-navi');
+    const widthOfNaviLayer = navLayer.offsetWidth;
+
+    if (window.innerWidth >= 768 && widthOfNaviLayer !== 0) {
+        navLayer.classList.remove('navi-layer-visible');
+        navLayer.classList.add('navi-layer-hidden');
+        naviBody.classList.remove('mobile-navi-visible');
+        naviBody.classList.add('mobile-navi-hidden');
+        headerNavi.classList.remove('mobile-header-navi-visible');
+        headerNavi.classList.add('mobile-header-navi-hidden');
+    }
+}
+
+document.getElementById('close-navi-icon').addEventListener('click', handleMobileNav);
+window.addEventListener('resize', handleResetMenuResize);
+document.getElementById('navi-mobile-button').addEventListener('click', handleMobileNav);
+
 document.getElementById('lens-mobile').addEventListener('click', handleSearchBox);
 document.getElementById('lens-desktop').addEventListener('click', handleSearchBox);
